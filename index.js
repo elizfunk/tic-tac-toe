d006cee6699cedc3f41c64bce7abf600b58920af
elizfunk <elizabeth_funk@hotmail.com>
2022-09-15 22:22:07 -0400

    initial verison of tic tac toe

---
+++ index.js
@@ -0,0 +1,228 @@
+function createStyledElement (className, element = 'div') {
+  const el = document.createElement(element)
+  el.setAttribute('class', className)
+  return el
+}
+
+class TicTacToe {
+  constructor() {
+    this.rows = 3
+    this.columns = 3
+    this.board = document.getElementById('board')
+    this.playerOne = {
+      letter: "X"
+    }
+    this.playerTwo = {
+      letter: "O"
+    }
+    this.boardData = this.createInitialBoardData()
+    this.activePlayer = this.playerOne
+    this.winner = null
+  }
+
+  updateIsWinner() {
+    // horizontal check
+    for (let i = 0; i < this.rows; i++) {
+      let winStack = []
+      let lastLetter = ''
+      for (let j = 0; j < this.columns; j++) {
+        if (this.boardData[i][j].letter) {
+          if (winStack.length === 0) {
+            winStack.push(this.boardData[i][j])
+            lastLetter = this.boardData[i][j].letter
+          } else if (this.boardData[i][j].letter === lastLetter) {
+            winStack.push(this.boardData[i][j])
+          } else {
+            winStack = [this.boardData[i][j]]
+            lastLetter = this.boardData[i][j].letter
+          }
+          if (winStack.length === 3) {
+            this.winner = lastLetter
+            const winnerEl = document.getElementById('winner')
+            winnerEl.innerText = `The winner is ${lastLetter}!`
+            return
+          }
+        } else {
+          winStack = []
+          lastLetter = ''
+        }
+      }
+    }
+
+    // vertical check
+    for (let i = 0; i < this.columns; i++) {
+      let winStack = []
+      let lastLetter = ''
+      for (let j = 0; j < this.rows; j++) {
+        if (this.boardData[j][i].letter) {
+          if (winStack.length === 0) {
+            winStack.push(this.boardData[j][i])
+            lastLetter = this.boardData[j][i].letter
+          } else if (this.boardData[j][i].letter === lastLetter) {
+            winStack.push(this.boardData[j][i])
+          } else {
+            winStack = []
+            lastLetter = ''
+          }
+          if (winStack.length === 3) {
+            this.winner = lastLetter
+            const winnerEl = document.getElementById('winner')
+            winnerEl.innerText = `The winner is ${lastLetter}!`
+            return
+          }
+        } else {
+          winStack = []
+          lastLetter = ''
+        }
+      }
+    }
+
+    // diagonal left to right check
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.columns; j++) {
+        let winStack = []
+        let lastLetter = ''
+        for (let k = i, m = j; k < this.rows && m < this.columns; k++, m++) {
+          if (this.boardData[k][m].letter) {
+            if (winStack.length === 0) {
+              winStack.push(this.boardData[k][m])
+              lastLetter = this.boardData[k][m].letter
+            } else if (this.boardData[k][m].letter === lastLetter) {
+              winStack.push(this.boardData[k][m])
+            } else {
+              winStack = [this.boardData[k][m]]
+              lastLetter = this.boardData[k][m].letter
+            }
+            if (winStack.length === 3) {
+              this.winner = lastLetter
+              const winnerEl = document.getElementById('winner')
+              winnerEl.innerText = `The winner is ${lastLetter}!`
+              return
+            }
+          }  else {
+            winStack = []
+            lastLetter = ''
+          }
+        }
+      }
+    }
+
+    // diagonal right to left check
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.columns; j++) {
+        let winStack = []
+        let lastLetter = ''
+        for (let k = i, m = j; k < this.rows && m >= 0; k++, m--) {
+          if (this.boardData[k][m].letter) {
+            if (winStack.length === 0) {
+              winStack.push(this.boardData[k][m])
+              lastLetter = this.boardData[k][m].letter
+            } else if (this.boardData[k][m].letter === lastLetter) {
+              winStack.push(this.boardData[k][m])
+            } else {
+              winStack = [this.boardData[k][m]]
+              lastLetter = this.boardData[k][m].letter
+            }
+            if (winStack.length === 3) {
+              this.winner = lastLetter
+              const winnerEl = document.getElementById('winner')
+              winnerEl.innerText = `The winner is ${lastLetter}!`
+            }
+          } else {
+            winStack = []
+            lastLetter = ''
+          }
+        }
+      }
+    }
+  }
+
+  createInitialBoardData() {
+    const tempBoard = []
+  
+    for (let row = 0; row < this.rows; row++) {
+      const currRow = []
+      for (let col = 0; col < this.columns; col++) {
+        currRow.push({
+          letter: null,
+        })
+      }
+      tempBoard.push(currRow)
+    }
+  
+    return tempBoard
+  }
+
+  handleClick = (event) => {
+    if (this.winner) {
+      window.alert(`The winner is ${this.winner}.  Please start a new game to continue playing.`)
+      return
+    }
+
+    const [pRow, pCol] = event.target.id.split('-')
+
+    const pieceIsEmpty = (this.boardData[pRow][pCol].letter === null)
+
+    if (pieceIsEmpty) {
+      event.target.innerText = `${this.activePlayer.letter}`
+      this.boardData[pRow][pCol].letter = this.activePlayer.letter
+
+      if (this.activePlayer === this.playerOne) {
+        this.activePlayer = this.playerTwo
+      } else {
+        this.activePlayer = this.playerOne
+      }
+
+      const activePlayerEl = document.getElementById('active-player')
+      activePlayerEl.innerText = `Active player: ${this.activePlayer.letter}`
+
+      this.updateIsWinner()
+      
+    } else {
+      window.alert("Only spaces that are empty and have no empty spaces below them can be played.")
+    }
+  }
+
+  renderBoard() {
+    for (let row = 0; row < this.rows; row++) {
+      const rowEl = createStyledElement('row')
+      for (let col = 0; col < this.columns; col++) {
+        const currRow = row
+        const currCol = col
+        const id = `${currRow}-${currCol}`
+
+        const pieceBackground = createStyledElement(`game-square`)
+        const piece = createStyledElement('square white-background', 'button')
+
+        piece.setAttribute('id', id)
+        piece.addEventListener('click', this.handleClick)
+
+        pieceBackground.append(piece)
+        rowEl.append(pieceBackground)
+      }
+      this.board.append(rowEl)
+    }
+  }
+}
+
+const startNewGame = () => {
+  const board = document.getElementById('board')
+  while (board.lastElementChild) {
+    board.removeChild(board.lastElementChild)
+  }
+
+  const winnerEl = document.getElementById('winner')
+  winnerEl.innerText = 'No winner yet'
+
+  const game = new TicTacToe
+  game.renderBoard()
+
+  const activePlayerEl = document.getElementById('active-player')
+  activePlayerEl.innerText = `Active player: ${game.activePlayer.letter}`
+}
+
+const startNewGameEl = document.getElementById('new-game')
+startNewGameEl.addEventListener('click', startNewGame)
+
+const game = new TicTacToe
+game.renderBoard()
---
+++ readme.md
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link href="styles.css" type="text/css" rel="stylesheet" />
+    <title>Tic Tac Toe - Let's Play a Game</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Tic Tac Toe</h1>
+      <h2 id="active-player">Active player: X</h2>
+      <h2 id="winner">No winner yet</h2>
+      <button id="new-game">Start a new game</button>
+      <div id="board"></div>
+    </div>
+    <script src="index.js" type="application/javascript">
+    </script>
+  </body>
+</html>
